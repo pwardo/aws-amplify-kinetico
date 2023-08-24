625789c13b067c85bbe0e70890d7db57025cae35
Patrick Ward <patwardo@gmail.com>
2023-08-24 15:45:01 +0100

    Copy in sample files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Footer from './component/Footer';
+import Guarantee from './component/Guarantee';
+import Header from './component/Header';
+import History from './component/History';
+import People from './component/People';
+import Products from './component/Products';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <History />
+      <Products />
+      <Guarantee />
+      <People />
+      <Footer />
     </div>
   );
 }
