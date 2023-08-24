c3f6a7ff672b4f155a66cc78491bc304062da33e
Patrick Ward <patwardo@gmail.com>
2023-08-24 17:50:06 +0100

    Add withAuthenticator from AWS Cognito to require authentication before viewing website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import Footer from './component/Footer';
 import Guarantee from './component/Guarantee';
 import Header from './component/Header';
@@ -7,9 +9,14 @@ import History from './component/History';
 import People from './component/People';
 import Products from './component/Products';
 
-function App() {
+function App({ signOut, user }) {
   return (
     <div className="App">
+      <div className="container">
+        <Heading level={1}>Hello {user.username}</Heading>
+        <Button onClick={signOut}>Sign out</Button>
+        <h2>Amplify Todos</h2>
+      </div>
       <Header />
       <History />
       <Products />
@@ -20,4 +27,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
